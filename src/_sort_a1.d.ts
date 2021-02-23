611fa1873213a0ededeb5ee90bc867be94fd1b51
Brian Takita <brian.takita@gmail.com>
2021-02-23 18:17:30 -0500

    update dependencies

--- src/_sort_a1.d.ts
+++ src/_sort_a1.d.ts
@@ -1,8 +1,8 @@
-import type { maybe } from '@ctx-core/function';
+import type { maybe, maybe_undefined } from '@ctx-core/function';
 import type { compare_type } from './compare_type';
 /**
  * Sorts a copy of `a1` by the `compare` function.
  */
 export declare function _sort_a1<I extends unknown = unknown>(a1: I[], compare?: compare_type<I>): I[];
-export declare function _maybe_sort_a1<I extends unknown = unknown>(maybe_a1: maybe<I[]>, compare?: compare_type<I>): maybe<I[], undefined>;
+export declare function _maybe_sort_a1<I extends unknown = unknown>(maybe_a1: maybe<I[]>, compare?: compare_type<I>): maybe_undefined<I[]>;
 export { _sort_a1 as _a1__sort, };
