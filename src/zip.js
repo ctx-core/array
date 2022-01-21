6de0c98d5dc706904e19697953b984e105cee983
Brian Takita <brian.takita@gmail.com>
2022-01-20 23:06:33 -0600

    formatting

--- src/zip.js
+++ src/zip.js
@@ -1,7 +1,8 @@
 import { zip_with } from './zip_with.js'
 /**
  * Returns 2d Array of each item being the index value for each Array in `...a2`.
- */ export function zip(a2) {
+ */
+export function zip(a2) {
 	return zip_with(a2)
 }
 export function maybe_zip(a2) {
